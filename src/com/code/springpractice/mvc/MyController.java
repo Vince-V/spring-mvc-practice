70bb67c4a86ce495637cf94e93e42e20a11d7ea4
Vince-V <vince2villas2@gmail.com>
2016-12-19 00:17:53 -0600

    First commit

---
+++ src/com/code/springpractice/mvc/MyController.java
@@ -0,0 +1,14 @@
+package com.code.springpractice.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping("/")
+	public String showPage(){
+		return "my-menu";
+		
+	}
+}
